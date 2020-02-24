e244837aaa25978d4654730c0a080eeda86649b8
David Esteban Vargas Quevedo <david.vargas@decameron.com>
2020-02-24 07:54:58 -0500

    Se crea el context para las recetas

--- src/context/RecetasContext.js
+++ src/context/RecetasContext.js
@@ -1,24 +1,47 @@
-import React, {createContext, useState} from "react";
+import React, {createContext, useState, useEffect} from "react";
+import axios from 'axios';
+import PropTypes from 'prop-types';
 
 export const RecetasContext = createContext();
 
 const RecetasProvider = (props) => {
 
-  const [recetas, guardarReceta] = useState([]);
+  const [recetas, guardarRecetas] = useState([]);
   const [busqueda, buscarRecetas] = useState({
     nombre: '',
     categoria: ''
   });
 
+  const [consultar, guardarConsultar] = useState(false);
+  const {nombre, categoria} = busqueda;
+
+  useEffect( () => {
+    if (consultar) {
+      const obtenerRecetas = async () => {
+        const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${nombre}&c=${categoria}`;
+
+        const recetas = await axios.get(url);
+        guardarRecetas(recetas.data.drinks);
+      };
+      obtenerRecetas();
+    }
+  }, [busqueda, consultar, nombre, categoria]);
+
   return (
     <RecetasContext.Provider
       value={{
-        buscarRecetas
+        recetas,
+        buscarRecetas,
+        guardarConsultar
       }}
     >
       {props.children}
     </RecetasContext.Provider>
   )
+};
+
+RecetasProvider.propTypes = {
+  props: PropTypes.object
 };
 
 export default RecetasProvider;
